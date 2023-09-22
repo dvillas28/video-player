d753911208323b1c7fc3a23e2cc70cf6d286c45c
Daniel Villasenor <dvillasc@gmail.com>
2023-09-22 19:21:51 -0300

    first commit

---
+++ video_player.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+pwd
+
+kill_vlc () {
+    # Check if VLC is running and kill it
+    if pgrep -x "vlc" > /dev/null; then
+        pkill -x "vlc"
+        # Adding a small sleep to ensure VLC fully closes before trying to open a new instance.
+        sleep 1
+    fi
+}
+
+declare -A videos
+
+# load video mappings from file
+while IFS="=" read -r key path; do
+    videos["$key"]="$path"
+# done < <(grep '=' Desktop/scripts/video_mappings.txt)
+done < <(grep '=' video_mappings2.txt)
+# done < <(grep '=' $1.txt) # file as an argument
+
+# echo the names
+for key in "${!videos[@]}"; do
+    video_path="${videos[$key]}"
+    video_name="$(basename "$video_path")" # extract filename
+    video_name_woutext="${video_name%.*}" # remove extension
+    
+    echo "> Key: $key, Video: $video_name_woutext"
+
+done
+
+echo ""
+echo "> Press a key to play a video (options: ${!videos[@]})"
+echo "> Press 'q' to quit the video" 
+echo "> Press 'k' to close the program"
+
+# main loop
+while true; do
+    # Read a single character of input
+    read -s -n 1 key
+
+    # If 'q' is pressed, exit the loop
+    if [[ $key == "k" ]]; then
+        echo "> Closing..."
+        
+        # Check if VLC is running and kill it
+        kill_vlc
+
+        break
+    fi
+
+    if [[ $key == "q" ]]; then
+        
+        # Check if VLC is running and kill it
+        kill_vlc
+    fi
+
+    if [[ $key == "p" ]]; then
+        
+        feh --fullscreen /home/pi/Desktop/photo.jpeg &
+    fi
+
+    # Check if VLC is running and kill it
+    kill_vlc
+
+    # Check if the key is in the videos associative array
+    if [[ ${videos[$key]} ]]; then
+        # vlc --fullscreen ${videos[$key]} --play-and-exit >/dev/null 2>&1 &
+        # vlc ${videos[$key]} --play-and-exit >/dev/null 2>&1 &
+        vlc ${videos[$key]} --play-and-exit &
+
+        # go back to terminal (only for GUI)
+        # sleep 1
+        # xdotool key alt+Tab
+    fi
+
+done
+
---
+++ wrapper.py
@@ -0,0 +1,67 @@
+"""
+Para controlar el script desde aqui
+"""
+
+import pygame
+import subprocess
+import os
+import time
+
+# Initialize pygame
+pygame.init()
+
+# Create a display surface (for the still image)
+screen = pygame.display.set_mode((800, 600))
+
+# Load your still image
+# image_path = "/path/to/your/still/image.jpg"
+# image = pygame.image.load(image_path)
+
+# Create a clock object to control frame rate
+clock = pygame.time.Clock()
+
+# Start the Bash script as a subprocess
+bash_process = subprocess.Popen(["./video_player.sh"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+# print(bash_process)
+
+# Function to display the still image
+def display_still_image():
+    # screen.blit(image, (0, 0))
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+
+# Main loop
+running = True
+display_still_image()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            key_pressed = chr(event.key)
+            print(key_pressed)
+
+            # Send the key to the Bash script
+            bash_process.stdin.write(key_pressed + "\n")
+            bash_process.stdin.flush()
+
+    # Check if the Bash script has finished
+    if bash_process.poll() is not None:
+        break
+
+    # Redraw the still image
+    display_still_image()
+
+    # Control frame rate
+    clock.tick(30)
+
+# Close the subprocess
+bash_process.stdin.close()
+bash_process.stdout.close()
+bash_process.stderr.close()
+bash_process.terminate()
+
+# Quit pygame
+pygame.quit()

94592ce013d1758db4ba0b1160c10548bc6cfd20
Daniel Villasenor <dvillasc@gmail.com>
2023-10-02 19:58:03 -0300

    añadida imagen de fondo

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Video Selector Player
-- Carga una playlist de videos que pueden ser reproducidos y cambiar de uno a otro con solo presionar una tecla
+# Video Selector Player :tv:
+- Carga una playlist de videos que pueden ser reproducidos y cambiar de uno a otro con solo presionar una tecla :keyboard:
 
 - Necesita un archivo `.txt` que contiene los *paths* de los videos a cargar. El path a este archivo debe ser ingresado como argumento
 
@@ -10,11 +10,17 @@ python3 video_selector_player.py <path/to/playlist.txt>
 ## Controles
 - Reproducir videos presionando una tecla numerica (`0`-`9`)
 
-- `X` para pausar, `C` para reanudar y `Z` para cerrar programa
+- `X` : para pausar reproductor
+- `C` : para reanudar reproductor
+- `space` : el video y cerrar el reproductor
+- `Z` : cerrar programa
 
-## TODOs
-- Colocar imagen predeterminada de fondo
+## TODOs ordenados por prioridad
 - Capacidad de reproducir un video predeterminado de fondo
-- Hacerlo frameless de algun modo - Done
-- Mantener Focus en el central Widget
 - Hasta el momento solo se pueden acceder los primeros 10 videos (teclas 0-9)
+- Mantener Focus en el central Widget
+
+## TODOs Listos
+- Hacerlo frameless de algun modo ✅
+- Colocar imagen predeterminada de fondo ✅
+- Cuando un video termina, el reproductor se cierra ✅
--- video_selector_player.py
+++ video_selector_player.py
@@ -1,94 +1,104 @@
-from PyQt6.QtCore import (Qt, QUrl)
-from PyQt6.QtWidgets import (QApplication, QWidget, QHBoxLayout)
+from PyQt6.QtCore import (Qt, QUrl, QTimer, pyqtSignal)
+from PyQt6.QtWidgets import (QApplication, QWidget, QHBoxLayout, QLabel)
 from PyQt6.QtMultimedia import (QAudioOutput, QMediaPlayer)
 from PyQt6.QtMultimediaWidgets import QVideoWidget
-from PyQt6.QtGui import QCursor
+from PyQt6.QtGui import QCursor, QPixmap, QKeyEvent
 import time
 import sys
 import os
 
 # TODO separar esto y hacerlo mas legible
 
-# TODO ocultar el widget reproductor si es que termina un video, esto se podria resolver si
-# en el archivo tenemos la duraccion de cada video
+
+class Video(QVideoWidget):
+    senal_load_and_play = pyqtSignal(int)
+    senal_pause = pyqtSignal()
+    senal_play = pyqtSignal()
+    senal_stop = pyqtSignal()
+    senal_stop_and_hide = pyqtSignal()
+
+    def onKeyPress_Event(self, event):
+        # self.activateWindow()
+        # self.parent().activateWindow()
+        # self.parent().raise_()
+
+        if chr(event.key()).isnumeric():
+            self.senal_load_and_play.emit(event.key())
+
+        # TODO juntar todo esto en un solo boton
+        elif event.key() == Qt.Key.Key_X:
+            """ Pausa el widget de video """
+            self.senal_pause.emit()
+
+        elif event.key() == Qt.Key.Key_C:
+            """ Continua la reproduccion del video """
+            self.senal_play.emit()
+
+        elif event.key() == Qt.Key.Key_Z:
+            """ Cierra la app """
+            self.senal_stop.emit()
+
+        elif event.key() == Qt.Key.Key_Space:
+            """ Detiene el video y esconde el widget """
+            if self.isVisible():
+                self.senal_stop_and_hide.emit()
 
 
 class MainWindow(QWidget):
+    key_pressed = pyqtSignal(QKeyEvent)
 
     def __init__(self, playlist_path):
         super().__init__()
+        # get the current size of the window
+        screen = QApplication.primaryScreen()
+        size = screen.size()
+
         # gui
-        self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
+        # self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
 
         self._audio_output = QAudioOutput(self)
         self._player = QMediaPlayer(self)
         self._player.setAudioOutput(self._audio_output)
 
-        self.layout = QHBoxLayout(self)
-        self._video_widget = QVideoWidget(self)
+        self._video_widget = Video(self)
+        self._video_widget.setGeometry(0, 0, size.width(), size.height())
         self._video_widget.showMaximized()
+        self._video_widget.setHidden(True)  # aparece oculto al inicio
 
-        # para poder controlarlo con las teclas inmediatamente al comenzar
-        # TODO mantener en todo momento un focus a la ventana exterior, de modo que
-        # los keypress event funcionen, buscar una manera de quitar el focus al widget
-        self._video_widget.setFocus()
+        # TODO, hallar la manera de siempre tener focus en la pantalla principal
         self._player.setVideoOutput(self._video_widget)
-        # self._video_widget.setFocusPolicy(Qt.FocusPolicy.StrongFocus)
-
-        # self._player.errorOccurred.connect(self._player_error)
-
-        # TODO, poner una imagen de fondo en el widget central, luego que los videos se superpongan
 
         # video info
         self._playlist_path = playlist_path
         self._videos = {}  # key y su path asociado
         self.load_videos()
+        self.curr_duration = None
+
+        # imagen de fondo
+        self.image_label = QLabel(self)
+        self.image_label.setGeometry(0, 0, size.width(), size.height())
+        path = os.path.join('images', 'example.jpg')
+        pixmap = QPixmap(path)
+        self.image_label.setPixmap(pixmap)
+        self.image_label.setScaledContents(True)
 
         # creamos un cursor y lo dejamos invisible
         self.cursor = QCursor()
         self.cursor.setShape(Qt.CursorShape.BlankCursor)
         QApplication.setOverrideCursor(self.cursor)
 
-        self.layout.addWidget(self._video_widget)
-        self.setLayout(self.layout)
+        self.showMaximized()
 
-    # def _player_error(self, error, error_string):
-    #     print(error_string, file=sys.stderr)
-    #     # self.show_status_message(error_string)
+    def connect_senales(self):
+        self.key_pressed.connect(self._video_widget.onKeyPress_Event)
 
-    def keyPressEvent(self, event):
-        # TODO repensar la implementacion de esto
-        # selected a video from the playlist
-        if chr(event.key()).isnumeric():
-            self.security_stop()
-            self.load_and_play(event.key())
+        self._player.mediaStatusChanged.connect(self.statusChanged)
 
-        elif event.key() == Qt.Key.Key_Z:
-            """ Cierra la app """
-            self._player.stop()
-            # QApplication.quit()
-            sys.exit()
-
-        # TODO juntar todo esto en un solo boton
-        elif event.key() == Qt.Key.Key_X:
-            """ Pausa el widget de video """
-            self._player.pause()
-
-        elif event.key() == Qt.Key.Key_C:
-            """ Continua la reproduccion del video """
-            self._player.play()
-
-    def load_and_play(self, key: int) -> None:
-        """ Se recibe un key, se carga y se reproduce un video """
-        path = self._videos[chr(key)]
-        url = QUrl.fromLocalFile(path)
-        self._player.setSource(url)
-        self._player.play()
-
-    def security_stop(self):
-        """ No funciona sin esto """
-        self._player.stop()
-        time.sleep(0.1)
+        self._video_widget.senal_load_and_play.connect(self.load_and_play)
+        self._video_widget.senal_pause.connect(self._player.pause)
+        self._video_widget.senal_play.connect(self._player.play)
+        self._video_widget.senal_stop.connect(self.stop_and_close)
+        self._video_widget.senal_stop_and_hide.connect(self.stop_and_hide)
 
     def load_videos(self):
         """ Lee el archivo y almacena las rutas en un diccionario"""
@@ -96,23 +106,64 @@ class MainWindow(QWidget):
         try:
             with open(os.path.join(self._playlist_path)) as arch:
                 lineas = arch.readlines()
+                info_list = []
                 for linea in lineas:
-                    linea = os.path.join(linea.strip())
+                    info_list.append(linea.strip().split(','))
 
                 c = 0
-                for linea in lineas:
-                    self._videos[str(c)] = linea
+                for video in info_list:
+                    video_path = os.path.join(video[0])
+                    dur = video[1]
+                    self._videos[str(c)] = (video_path, int(dur))
                     c += 1
 
         except FileNotFoundError:
             print(f'> Error: "{self._playlist_path}" is not a valid path')
             sys.exit()
 
+    def keyPressEvent(self, event):
+        self.key_pressed.emit(event)
+        return super().keyPressEvent(event)
+
+    def load_and_play(self, key: int) -> None:
+        """ Se recibe un key, se carga y se reproduce un video """
+        try:
+            path, duration = self._videos[chr(key)]
+
+        except KeyError:
+            print(f'> Error: Key "{chr(key)}" is not registered to any video')
+
+        else:
+            self._video_widget.setHidden(False)
+            self.security_stop()
+            self.curr_duration = duration
+            url = QUrl.fromLocalFile(path)
+            self._player.setSource(url)
+            self._player.play()
+
+            print(f'> Now Playing -> {path}')
+
+    def security_stop(self):
+        """ No funciona sin esto """
+        self._player.stop()
+        time.sleep(0.1)
+
+    def stop_and_hide(self):
+        # the player is stopped and the widget is hidden
+        self._player.stop()
+        self._video_widget.setHidden(True)
+
+    def stop_and_close(self):
+        self._player.stop()
+        sys.exit()
+
+    def statusChanged(self, status):
+        if status == self._player.MediaStatus.EndOfMedia:
+            self.stop_and_hide()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     main_win = MainWindow(sys.argv[1])
-    available_geometry = main_win.screen().availableGeometry()
-    main_win.resize(300, 300)
-    main_win.showMaximized()
+    main_win.connect_senales()
     sys.exit(app.exec())
